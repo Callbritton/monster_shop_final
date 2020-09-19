5ad6eaa3ecd94717797b864b3c800859cbaa71dd
Chris Allbritton <cla.inbox@gmail.com>
2020-09-19 16:38:59 -0400

    Add discounts controller and index action

--- spec/features/merchant/discounts/index_spec.rb
+++ spec/features/merchant/discounts/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "As a merchant employee" do
   before :each do
     @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
     @m_user = @megan.users.create!(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword', role: 1)
-    discount = @megan.discounts.create!(name: "10 Off!", percent: 10, minimum_quantity: 10)
+    discount = @megan.discounts.create!(name: "10% Off!", percent: 10, minimum_quantity: 10)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
   end
 
@@ -13,6 +13,6 @@ RSpec.describe "As a merchant employee" do
     click_on "Current Discounts"
 
     expect(current_path).to eq("/merchant/discounts")
-    expect(page).to have_content("10 Off!")
+    expect(page).to have_content("10% off when you buy 10 items!")
   end
 end
