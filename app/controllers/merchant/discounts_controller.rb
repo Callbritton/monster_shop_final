c7ee31e551b310c553eeddfbf0fa363f150e1058
Chris Allbritton <cla.inbox@gmail.com>
2020-09-19 20:03:35 -0400

    Add edit and update actions to discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -14,8 +14,24 @@ class Merchant::DiscountsController < Merchant::BaseController
       flash[:success] = "Your discount has been created"
       redirect_to "/merchant/discounts"
     else
-      flash[:notice] = "Something has gone wrong, please try again"
+      flash[:notice] = "All fields must be completed. Please try again."
       redirect_to "/merchant/discounts/new"
+    end
+  end
+
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+    discount = Discount.find(params[:id])
+    discount.update(discount_params)
+    if discount.save
+      flash[:success] = "Discount has been updated!"
+      redirect_to "/merchant/discounts"
+    else
+      flash[:notice] = "All fields must be completed. Please try again."
+      redirect_to "/merchant/discounts/#{discount.id}/edit"
     end
   end
 
