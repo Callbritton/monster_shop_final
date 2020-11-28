b044a3ebc50b03e9173d31fa228564742b6affbd
Chris Allbritton <cla.inbox@gmail.com>
2020-11-28 11:48:10 -0500

    Refactor cart controller to previous iteration after route refactor

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,7 +1,7 @@
 class CartController < ApplicationController
   before_action :exclude_admin
 
-  def create
+  def add_item
     item = Item.find(params[:item_id])
     session[:cart] ||= {}
     if cart.limit_reached?(item.id)
@@ -23,17 +23,17 @@ class CartController < ApplicationController
     redirect_to '/cart'
   end
 
-  def destroy
-    session[:cart].delete(params[:id])
+  def remove_item
+    session[:cart].delete(params[:item_id])
     redirect_to '/cart'
   end
 
-  def update
+  def update_quantity
     if params[:change] == "more"
-      cart.add_item(params[:id])
+      cart.add_item(params[:item_id])
     elsif params[:change] == "less"
-      cart.less_item(params[:id])
-      return destroy if cart.count_of(params[:id]) == 0
+      cart.less_item(params[:item_id])
+      return remove_item if cart.count_of(params[:item_id]) == 0
     end
     session[:cart] = cart.contents
     redirect_to '/cart'
